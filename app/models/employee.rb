c0491a0329e426bd5706e3b5bf7fce004a89efd3
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2012-12-12 19:42:23 -0600

    Generated CSV string in Employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -17,4 +17,15 @@ class Employee < ActiveRecord::Base
   def self.total_amount
     Employee.all.map(&:salary).sum
   end
+
+  def self.csv_report
+    string_report = CSV.generate do |row|
+      row << ["ID", "Fullname",	"Account number",	"Rate",	"Hours week",	"hours worked",	"Salary"]
+      Employee.all.each do |employee|
+        row << [employee.id, "#{employee.firstname} #{employee.lastname}", employee.account_number, employee.rate, employee.hours_week, employee.worked_hours, employee.salary]
+      end
+      row << [" "," "," "," "," ","Total: ", Employee.total_amount]
+    end
+    string_report
+  end
 end
