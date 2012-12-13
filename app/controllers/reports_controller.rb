32d84c48cb181f018ead5a98c8ccd83992f67dac
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2012-12-12 19:42:58 -0600

    Added CSV format to render with index action on reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,5 +2,9 @@ class ReportsController < ApplicationController
   def index
     @employees = Employee.all
     @total_amount = Employee.total_amount
+    respond_to do |format|
+      format.html
+      format.csv { render text: Employee.csv_report }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 UserReport::Application.routes.draw do
   root to: 'reports#index'
 
-  resources :employees, only: [:new, :create, :destroy, :edit, :update]
+  resources :employees, only: [:new, :create, :destroy, :edit, :update] 
+
+  resources :reports, only: [:index]
 end
