1583a64b18ad1df5e0184eb843eadb59543c1b83
Mayra Canela <mayra.canela@tangosource.com>
2013-01-03 12:19:53 -0600

    Added links in the devise application

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,6 @@ module UserReport
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    UserReport::Application.config.secret_token = '4823bf13b31a9c5bfb88483017bab440a1e81b21b5c5ad1b47006ce91229841a5d06eaabfb9e53bff6cbb580edd2ce7f21925823cab62e680f9bd6f3dc81efeb'
   end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-UserReport::Application.config.secret_token = '4823bf13b31a9c5bfb88483017bab440a1e81b21b5c5ad1b47006ce91229841a5d06eaabfb9e53bff6cbb580edd2ce7f21925823cab62e680f9bd6f3dc81efeb'
+
